02c95837b9629ea37a98edd4cab2df8f053d3246
Sio Jurnalis Pipin <siojurnalispipin@hotmail.com>
2021-12-12 16:03:33 +0700

    up

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -5,10 +5,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      projectId: 'personal-site-76e82',
+      projectId: 'jurnalsio',
     }),
     databaseURL:
-      'https://personal-site-76e82-default-rtdb.europe-west1.firebasedatabase.app/',
+      'https://jurnalsio-default-rtdb.asia-southeast1.firebasedatabase.app/',
   });
 }
 
